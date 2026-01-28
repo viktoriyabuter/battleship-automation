99c0101f161be9a9848d0edf9b82002a1cdcbaab
viktoriyabuter <viktoriya.buter@gmail.com>
2026-01-28 16:52:39 +0300

    added run debug

--- application/run_battleship.py
+++ application/run_battleship.py
@@ -1,24 +1,28 @@
 from playwright.sync_api import sync_playwright
+from config.settings import BASE_URL
+from infrastructure.ui.playwright.pages.start_page import StartPage
+from infrastructure.ui.playwright.pages.placement_page import PlacementPage
 from infrastructure.ui.playwright.pages.battle_page import BattlePage
 from application.game_runner import run_game
-from config.settings import BASE_URL
 
 
 def main():
     with sync_playwright() as pw:
         browser = pw.chromium.launch(headless=False)
         page = browser.new_page()
-
         page.goto(BASE_URL)
 
-        battle_page = BattlePage(page)
+        start_page = StartPage(page)
+        start_page.select_random_opponent()
+        start_page.start_game()
 
-        try:
-            run_game(battle_page)
-        except Exception as e:
-            print(f"Ошибка во время игры: {e}")
-        finally:
-            browser.close()
+        placement_page = PlacementPage(page)
+        placement_page.confirm_placement()  # просто заглушка
+
+        battle_page = BattlePage(page)
+        run_game(battle_page)
+
+        browser.close()
 
 
 if __name__ == "__main__":
