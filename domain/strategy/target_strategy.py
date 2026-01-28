b5b7b90c92dbaec622f9650fcc34de1a9a030063
viktoriyabuter <viktoriya.buter@gmail.com>
2026-01-28 16:15:10 +0300

    added mock strategies

---
+++ domain/strategy/target_strategy.py
@@ -0,0 +1,17 @@
+from typing import Tuple
+from domain.enums.shot_result import ShotResult
+from .base_strategy import BaseStrategy
+
+Coordinate = Tuple[int, int]
+
+class TargetStrategy(BaseStrategy):
+    """
+    Заглушка для стратегии добивания.
+    Пока просто наследуется от BaseStrategy.
+    """
+
+    def choose_next_shot(self) -> Coordinate:
+        raise NotImplementedError("TargetStrategy пока не реализована")
+
+    def register_result(self, coord: Coordinate, result: ShotResult) -> None:
+        pass
