e62b72e86fbd63b06977fc915c84d2394b5a0161
viktoriyabuter <viktoriya.buter@gmail.com>
2026-01-28 17:08:52 +0300

    start page locators debug

--- infrastructure/ui/playwright/pages/start_page.py
+++ infrastructure/ui/playwright/pages/start_page.py
@@ -1,13 +1,19 @@
 from playwright.sync_api import Page
-from config import settings
 
 
 class StartPage:
+    RANDOM_OPPONENT_BUTTON = (
+        "a.battlefield-start-choose_rival-variant-link:has-text('случайный')"
+    )
+    START_GAME_BUTTON = "div.battlefield-start-button"
+
     def __init__(self, page: Page):
         self.page = page
 
     def select_random_opponent(self) -> None:
-        self.page.click(f"text={settings.BUTTON['random_opponent']}")
+        self.page.wait_for_selector(self.RANDOM_OPPONENT_BUTTON, timeout=30_000)
+        self.page.click(self.RANDOM_OPPONENT_BUTTON)
 
     def start_game(self) -> None:
-        self.page.click(f"text={settings.BUTTON['start_game']}")
+        self.page.wait_for_selector(self.START_GAME_BUTTON, timeout=30_000)
+        self.page.click(self.START_GAME_BUTTON)
