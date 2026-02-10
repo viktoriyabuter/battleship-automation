92d100275d5c378cc7d278fd51e343a58283b9a7
viktoriyabuter <viktoriya.buter@gmail.com>
2026-02-10 15:29:34 +0300

    strategy resolver debug

--- domain/strategy/strategy_resolver.py
+++ domain/strategy/strategy_resolver.py
@@ -1,14 +1,12 @@
 from .base_strategy import BaseStrategy
 from .chessboard_strategy import ChessboardStrategy
 from .target_strategy import TargetStrategy
-from .probability_strategy import ProbabilityStrategy
 
 
 class StrategyResolver:
     def __init__(self):
         self.chessboard_strategy = ChessboardStrategy()
         self.target_strategy = TargetStrategy()
-        self.probability_strategy = ProbabilityStrategy()
 
     def get_strategy(self) -> BaseStrategy:
         if self.chessboard_strategy.hit_cells:
