af1416b528a23c187d88d0994a7ffb70da50954f
viktoriyabuter <viktoriya.buter@gmail.com>
2026-02-11 14:52:20 +0300

    architecture refactor

--- infrastructure/ui/playwright/pages/placement_page.py
+++
@@ -1,9 +0,0 @@
-from playwright.sync_api import Page
-
-
-class PlacementPage:
-    def __init__(self, page: Page):
-        self.page = page
-
-    def confirm_placement(self) -> None:
-        pass
